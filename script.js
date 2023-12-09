43cb61ecd4789a5cb14df2c248747b496af31950
shalvazak <shalva.zak@gmail.com>
2023-12-09 15:00:59 +0400

    added js

--- script.js
+++ script.js
@@ -1,11 +1,71 @@
-function myFunc (a,b, ...c) {
-    const arr = []
-    let count = 0 
-    arr[0] = a + b;
-    c.map (e => count += e )
-    arr[1] = count
-    console.log(arr);
-}
+// =================================================================
+// დავალება 1
+// =================================================================
 
-myFunc (3, 4, 5, 6, 7, 8);
-myFunc (5, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+// function myFunc (a,b, ...c) {
+//     const arr = []
+//     let sum = 0 
+//     arr[0] = a + b;
+//     c.forEach (e => sum += e )
+//     arr[1] = sum
+//     console.log(arr);
+// }
+
+// myFunc (3, 4, 5, 6, 7, 8);
+// myFunc (5, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+// =================================================================
+// დავალება 2
+// =================================================================
+
+// const user = {
+//     banks : [
+//         {
+//             address: {city: "Telavi", street: "Davitashvili"}
+//         },
+//         {
+//             address: {city: "Kutaisi", street: "Tavisufleba"} 
+//         },
+//         {
+//             address: {city: "Tbilisi", street: "Rustaveli"}
+//         }
+//     ]
+// }
+
+// const getCity = ({banks : [, , {address : {city} = {}} = {}]}) => city
+// console.log(getCity(user));
+
+// =================================================================
+// დავალება 3
+// =================================================================
+
+// const test_obj = {
+//     banks : [
+//         {
+//             address: {city: "Telavi", street: "Davitashvili"}
+//         },
+//         {
+//             address: {city: "Kutaisi", street: "Tavisufleba"} 
+//         },
+//         {
+//             address: {city: "Tbilisi", street: "Rustaveli"}
+//         }
+//     ]
+// }
+
+// function objClone (obj) {
+//     let new_obj = {}
+
+//     new_obj = {
+//         banks : [
+//             {address : {...test_obj.banks[0].address}},
+//             {address : {...test_obj.banks[1].address}},
+//             {address : {...test_obj.banks[2].address}},
+//         ]
+//     }
+
+//     new_obj.banks[0].address.city = 'eroor'
+//     return (new_obj);
+// }
+
+// console.log(objClone (test_obj));
